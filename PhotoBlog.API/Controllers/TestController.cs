1d4a4efda06af36f3a153a91398da399af4fdadd
Danny Redden <dannyredden18@gmail.com>
2025-06-09 21:03:26 -0400

    docker stuff

---
+++ PhotoBlog.API/Controllers/TestController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.OpenApi;
+
+namespace PhotoBlog.API.Routes;
+
+public static class TestRoutes
+{
+    public static IEndpointRouteBuilder MapTestRoutes(this IEndpointRouteBuilder app)
+    {
+        string[] photos = ["Photo 1", "Photo 2", "Photo 3"];
+        var random = new Random();
+
+        app.MapGet("/random-photo", () =>
+        {
+            var photo = photos[random.Next(photos.Length)];
+            return Results.Ok(new { photo });
+        })
+        .WithName("GetRandomPhoto");
+
+        return app;
+    }
+}
--- PhotoBlog.API/Program.cs
+++ PhotoBlog.API/Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.OpenApi.Models;
+using PhotoBlog.API.Routes;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PhotoBlog API", Version = "v1" });
+});
 
 var app = builder.Build();
 
@@ -18,8 +24,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
 
-app.MapControllers();
+app.MapTestRoutes();
 
 app.Run();
