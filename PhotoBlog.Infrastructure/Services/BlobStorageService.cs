f0ae13bca02a397256cdd6bde7a3fd33b9fe380f
Danny Redden <dannyredden18@gmail.com>
2025-06-10 21:14:42 -0400

    added nuget packages and some basic blob storage stuff

---
+++ PhotoBlog.Infrastructure/Services/BlobStorageService.cs
@@ -0,0 +1,24 @@
+﻿
+using Azure.Storage.Blobs;
+
+namespace PhotoBlog.Infrastructure.Services;
+
+public class BlobStorageService
+{
+    private readonly BlobContainerClient _containerClient;
+
+    //public BlobStorageService(IConfiguration config)
+    //{
+    //    var connectionString = config["AzureBlob:ConnectionString"];
+    //    var containerName = config["AzureBlob:Container"];
+    //    _containerClient = new BlobContainerClient(connectionString, containerName);
+    //}
+
+    //public async Task<string> UploadAsync(IFormFile file)
+    //{
+    //    var blobClient = _containerClient.GetBlobClient(file.FileName);
+    //    await using var stream = file.OpenReadStream();
+    //    await blobClient.UploadAsync(stream, overwrite: true);
+    //    return blobClient.Uri.ToString();
+    //}
+}
